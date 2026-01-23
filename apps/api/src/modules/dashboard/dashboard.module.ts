9872d13b9e7accf6d38cf45d71c3fb2ae7c61092
Abdu Samad <AbduSamad.pv.95@gmail.com>
2026-01-23 15:18:28 +0400

    fix: include auth and rbac in dashboard module

--- apps/api/src/modules/dashboard/dashboard.module.ts
+++ apps/api/src/modules/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
+import { RbacModule } from "../../rbac/rbac.module";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { DashboardController } from "./dashboard.controller";
 import { DashboardService } from "./dashboard.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [AuthModule, RbacModule, PrismaModule],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
